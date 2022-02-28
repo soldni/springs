89cfd343c13b58c254cf3077e9f9788bccfafd91
Luca Soldaini <lucas@allenai.org>
2022-02-28 12:33:27 -0800

    bumped version, release script

---
+++ scripts/push-to-pypi.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# get script directory
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  # if $SOURCE was a relative symlink, we need to resolve it
+  # relative to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$SCRIPT_DIR/$SOURCE"
+done
+SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+# this is the directory i'm in rn
+CURRENT_DIR=$(pwd)
+
+# fail if any command fails
+set -ex
+
+# upgrade twine as needed
+python3 -m pip install --upgrade build twine
+
+# moves up to root dir
+cd "${SCRIPT_DIR}/.."
+
+# build and upload to PyPi
+python3 -m build
+python3 -m twine upload dist/*
+
+# go back to original dir
+cd ${CURRENT_DIR}
