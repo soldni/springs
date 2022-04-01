b9a21525b7a4fb6c492b59aefba74236654c7710
Luca Soldaini <lucas@allenai.org>
2022-03-31 18:45:17 -0700

    forgot to update multi type

--- src/espresso_config/experimental.py
+++ src/espresso_config/experimental.py
@@ -4,11 +4,47 @@ from typing import Any, Sequence, Tuple, Type, Union
 from .node import ConfigNode, ConfigParam
 
 
+class _MultiType:
+    def __init__(self, types):
+        self.types = types
+
+    def __instancecheck__(cls, __instance: Any) -> bool:
+        return isinstance(__instance, cls.types)
+
+    def __str__(self):
+        types_repr = ", ".join(repr(t) for t in self.types)
+        return f'{self.__class__.__name__}({types_repr})'
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __call__(self, to_cast):
+        if not isinstance(to_cast, self.types):
+
+            # this is used to throw an exception if we
+            # cant cast. We can't immediately throw an exception,
+            # we need to try to cast to all types before giving up
+            exception: Union[Exception, None] = None
+
+            # we try to cast one type at the time
+            for t in self.types:
+                try:
+                    # we immediately return in case
+                    # casting is successful
+                    return t(to_cast)
+                except Exception as e:
+                    exception = e
+
+            if exception is not None:
+                raise exception
+
+
+
 class ConfigParamMultiType(ConfigParam):
     """A ConfigParameter that accepts multiple types.
     casting to parameters is resolved in the order they
     are provided."""
-    def __new__(cls, *target_types: Sequence[Type]):
+    def __init__(self, *target_types: Sequence[Type]):
         # in case target types is an iterable
         target_types = tuple(t for t in target_types)
 
@@ -18,54 +54,8 @@ class ConfigParamMultiType(ConfigParam):
         if any(inspect.isclass(t) and issubclass(t, ConfigNode)
                for t in target_types):
             # TODO: support nested configs
-            msg = (f'{cls.__name__} does not currently accept ConfigNode'
-                   ' as one of the provided types.')
+            msg = (f'{type(self).__name__} does not currently accept '
+                    'ConfigNode as one of the provided types.')
             raise ValueError(msg)
 
-        class MultiTypeMeta(type):
-            types: Tuple[Type]
-
-            def __instancecheck__(cls, __instance: Any) -> bool:
-                return isinstance(__instance, cls.types)
-
-            def __subclasscheck__(cls, __subclass: type) -> bool:
-                return issubclass(__subclass, cls.types)
-
-
-        class MultiType(metaclass=MultiTypeMeta):
-            types = target_types
-
-            def __str__(self):
-                types_repr = ", ".join(repr(t) for t in self.types)
-                return f'{self.__class__.__name__}(types_repr)'
-
-            def __repr__(self):
-                return self.__str__()
-
-            def __new__(cls, to_cast):
-                if not isinstance(to_cast, cls.types):
-
-                    # this is used to
-                    exception: Union[Exception, None] = None
-
-                    # we try to cast one type at the time,
-                    # saving
-                    for t in cls.types:
-                        try:
-                            # we immediately return in case
-                            # casting is successful
-                            return t(to_cast)
-                        except Exception as e:
-                            exception = e
-
-                    if exception is not None:
-                        raise exception
-
-        target_type_repr = ', '.join(t.__name__ for t in target_types)
-
-        class ConfigTypedParam(cls):
-            # the call to type fn is hacky, but allows this class to
-            # have a custom name that includes the types
-            type = type(f'MultiType({target_type_repr})', (MultiType, ), {})
-
-        return ConfigTypedParam
+        self.type = _MultiType(target_types)
