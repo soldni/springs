03bfc66aedad2ae778afc838549edae97aadfddd
Luca Soldaini <luca@soldaini.net>
2022-09-02 15:32:27 -0700

    added a test to make sure that flexyclasses are pickable

---
+++ tests/test_pickle.py
@@ -0,0 +1,29 @@
+import pickle
+import unittest
+
+import springs as sp
+
+
+@sp.dataclass
+class ConfigA:
+    a: int = 1
+
+
+@sp.make_flexy
+@sp.dataclass
+class ConfigB:
+    b: int = 2
+
+
+class TestPickle(unittest.TestCase):
+    def test_fixed_pickle(self):
+        c = sp.from_dataclass(ConfigA)
+        c_ = pickle.loads(pickle.dumps(c))
+
+        self.assertEqual(c, c_)
+
+    def test_flexy_pickle(self):
+        c = sp.from_dataclass(ConfigB)
+        c_ = pickle.loads(pickle.dumps(c))
+
+        self.assertEqual(c, c_)
